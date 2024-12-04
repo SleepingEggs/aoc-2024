69fc3273db39fb535b9fb9f1770f4fc0b5b44ae9
aron <aronpalic@hotmail.com>
2024-12-04 23:31:16 +1100

    day1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 use std::env;
+use aocutils::timer;
+
 fn main() -> Result<()> {
+    let _timer = timer::Timer::new();
     let args: Vec<String> = env::args().collect();
     let filename = &args.get(1).expect("Please provide one argument");
     let file = File::open(filename)?;
@@ -17,6 +21,12 @@ fn main() -> Result<()> {
 
     if cfg!(feature = "part2") {
         println!("not implemented yet");
+        let mut my_map: HashMap<&i32, i32> = HashMap::new();
+        for entry in &right_list {
+            *my_map.entry(entry).or_insert(0) += 1;
+        }
+        let result = left_list.into_iter().fold(0, |acc, a| acc + a * my_map.get(&a).or(Some(&0)).unwrap());
+        println!("result {}", result);
     }
     else {
         left_list.sort();
